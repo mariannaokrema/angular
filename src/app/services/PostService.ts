080e3ea7e602937c63c14d5d89ea480fbc84661b
marianna <okremama@gmail.com>
2024-07-19 16:55:43 +0300

    changed any type

--- src/app/services/PostService.ts
+++ src/app/services/PostService.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { TResponseData } from '../types/responseData';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ export class PostService {
 
   private readonly http: HttpClient = inject(HttpClient);
 
-  createPost(data: any) {
+  createPost(data: TResponseData) {
     return this.http.post(this.apiUrl, data);
   }
 }
